7ab8f7676df0e76e6a90b66af9a5b94344209825
hsiangfeng <loveyou520babytw@gmail.com>
2019-12-13 22:10:01 +0800

    add: express

---
+++ serve.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.static('public')); // 設置靜態資源目錄
+app.get('/', (req, res) => {
+  res.sendfile('./public/');
+});
+
+app.listen(process.env.PORT || 3000);
