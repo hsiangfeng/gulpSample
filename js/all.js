5d7a0e2737b9d93e27a41896bb226257eee30474
hsiangfeng <loveyou520babytw@gmail.com>
2019-12-19 14:51:49 +0800

    Update 2019-12-19T06:51:46.166Z

--- js/all.js
+++ js/all.js
@@ -1,2 +1,21 @@
-"use strict";var a=function(){return console.log("小王好爛")},b=1234;setTimeout(function(){console.log("321 倒著脫")},5e3);a=function(){return console.log("小明好棒")},b=1234;setTimeout(function(){console.log("123 站著穿")},5e3);
+"use strict";
+
+var a = function a() {
+  return console.log('小王好爛');
+};
+
+var b = 1234;
+setTimeout(function () {
+  console.log('321 倒著脫');
+}, 5000);
+"use strict";
+
+var a = function a() {
+  return console.log('小明好棒');
+};
+
+var b = 1234;
+setTimeout(function () {
+  console.log('123 站著穿');
+}, 5000);
 //# sourceMappingURL=all.js.map
